8356308db8dcd2229d6b89e08a8876ba5bce2ec3
Arda Cicek <ardacck@gmail.com>
2016-12-12 14:38:21 +0300

    fix indent

--- lib/subconvtr.rb
+++ lib/subconvtr.rb
@@ -1,31 +1,31 @@
 module Subconvtr
 
-	class Monitor
+  class Monitor
 
-		# Converts the target file from windows-1254 to utf-8 encoding.
-		def run
+    # Converts the target file from windows-1254 to utf-8 encoding.
+    def run
 
-			output = `ls | grep -E ".srt"`.split("\n")
+      output = `ls | grep -E ".srt"`.split("\n")
 
-			# puts output
-			output.each do |current|
-				if !(`file -I "#{current}"`.include? "charset=utf-8")
-					puts `file -I "#{current}"`
-					`iconv -f windows-1254 -t utf-8 "#{current}" > temp24124eqqweqwrfsdf.srt && mv temp24124eqqweqwrfsdf.srt "#{current}"`
-					puts `file -I "#{current}"`
-				else
-					print '.'
-				end
-			end
-			
-			sleep 1
-		end
+      # puts output
+      output.each do |current|
+        if !(`file -I "#{current}"`.include? "charset=utf-8")
+          puts `file -I "#{current}"`
+          `iconv -f windows-1254 -t utf-8 "#{current}" > temp24124eqqweqwrfsdf.srt && mv temp24124eqqweqwrfsdf.srt "#{current}"`
+          puts `file -I "#{current}"`
+        else
+          print '.'
+        end
+      end
 
-		def self.start
-			loop do
-			self.new.run
-			end
-		end
-	end
+      sleep 1
+    end
+
+    def self.start
+      loop do
+        self.new.run
+      end
+    end
+  end
 end
 Subconvtr::Monitor.start
