51e2b9df1b349bd5b2ee4c5b8668d134288c23ee
Arda Cicek <ardacck@gmail.com>
2016-12-12 14:33:02 +0300

    indentation

--- lib/subconvtr.rb
+++ lib/subconvtr.rb
@@ -1,33 +1,31 @@
 module Subconvtr
+
 	class Monitor
 
 		# Converts the target file from windows-1254 to utf-8 encoding.
-	  def run
-	    # puts "merhaba #{target_file_name}!"
-			#file = #{target_file_name}	\
-			# nice = `iconv -f windows-1254 -t utf-8 "#{target_file_name}" > temp24124eqqweqwrfsdf.srt && mv temp24124eqqweqwrfsdf.srt "#{target_file_name}" && rm temp24124eqqweqwrfsdf.srt`
-			# puts 'OUTPUT: ' + output
+		def run
 
-	    output = `ls | grep -E ".srt"`.split("\n")
-	    # puts output
-	    output.each do |current|
+			output = `ls | grep -E ".srt"`.split("\n")
 
+			# puts output
+			output.each do |current|
 				if !(`file -I "#{current}"`.include? "charset=utf-8")
 					puts `file -I "#{current}"`
-		    	`iconv -f windows-1254 -t utf-8 "#{current}" > temp24124eqqweqwrfsdf.srt && mv temp24124eqqweqwrfsdf.srt "#{current}"`
-		    	puts `file -I "#{current}"`
-		    else
-		    	print '.'
-		    end
+					`iconv -f windows-1254 -t utf-8 "#{current}" > temp24124eqqweqwrfsdf.srt && mv temp24124eqqweqwrfsdf.srt "#{current}"`
+					puts `file -I "#{current}"`
+				else
+					print '.'
+				end
 			end
+			
 			sleep 1
-	  end
+		end
 
 		def self.start
-		  loop do
-		    self.new.run
-		   end
-		 end
+			loop do
+			self.new.run
+			end
+		end
 	end
 end
 Subconvtr::Monitor.start
